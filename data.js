2adce7d7fab83f34b7e40c3541d6ba8e4b143bb0
paizonx <paizonx@gmail.com>
2026-02-20 07:55:00 +0700

    Add files via upload

---
+++ data.js
@@ -0,0 +1,20 @@
+// ==========================================
+// DATABASE VIDEO
+// Masukkan ID Video Videy Anda di sini.
+// Bisa dengan ekstensi (contoh: "abc.mp4", "xyz.mov") 
+// atau tanpa ekstensi (contoh: "abc").
+// ==========================================
+
+const videoList = [
+    "aqwvCVWv1",
+    "QlXarW9J1",
+    "9bgq8gbW1",
+    "yzgarDr91",
+    "T0qxWRCe1",
+    "eBYn6LJV1",
+	"nwnk9H0y1",
+    "oRjlHAto1",
+    "NeQusurl1",
+    "0uJcA7hW1",
+    "zVibXvsu1",
+];
